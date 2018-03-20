ccce970e235e14d85134911f395c30206e6506ff
Jonathan Schiefer <jonathan.schiefer@posteo.de>
2018-03-20 14:28:09 +0100

    added licence information

--- simulation_functions.py
+++ simulation_functions.py
@@ -3,7 +3,7 @@
 
 
 #Estimate Connections from inverse covariance
-#Copyright (C) 2018  Jonathan Schiefer, Alexander Niederbühl, Volker Pernice
+#Copyright (C) 2018  Jonathan Schiefer
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
